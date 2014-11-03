811d43f7ce0472b2fd9a443c94ec7661bfe9c6fd
shihjie <shihjie1123@hotmail.com>
2014-11-03 22:57:33 +0800

    Update register.php

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 <table align=center border=0 cellpadding=5px>
 <tr>
 <td>Username</td>
-<td><input type="text" size="15" maxlength="15" name="username"></td>
+<td><input type="text" size="15" maxlength="15" name="username" onkeyup="checkAvail()"></td>
 <!--I will add a availability checker later-->
 </tr>
 <tr>
@@ -12,32 +12,45 @@
 </tr>
 <tr>
 <td>Password Confirmation</td>
-<td><input type="text" size="15" maxlength="15" name="pwconfirm"></td>
+<td><input type="password" size="15" maxlength="15" name="pwconfirm"></td>
 <!--Also password confirmation-->
+</tr>
+<tr>
+<td>First Name</td>
+<td><input type="text" size="15" maxlength="15" name="fname"></td>
+</tr>
+<tr>
+<td>Last Name</td>
+<td><input type="text" size="15" maxlength="15" name="lname"></td>
 </tr>
 <tr>
 <td>Email Address</td>
 <td><input type="text" size="30" maxlength="30" name="email"></td>
 </tr>
 <tr>
+<td>Address</td>
+<td><input type="text" size="50" maxlength="50" name="address"></td>
+</tr>
+<tr>
 <td>Contact Number</td>
 <td><input type="text" size="15" maxlength="15" name="number"></td>
 </tr>
 <tr>
-<td>Payment Method</td>
+<td>Security Question</td>
 <td>
-<select name="paymentmethod">
-<option value="visa">Visa</option>
-<option value="master">Master Card</option>
-<option value="a_ex">American Express</option>
+<select name="securityquestion">
+<option value="favcolor">What is your favourite colour?</option>
+<option value="elementaryschool">What is the name of your elementary/primary school?</option>
+<option value="petname">What is your pet's name?</option>
+<option value="nickname">What is your nickname?</option>
+<option value="bestfren">What is the name of your bestfriend?</option>
 </select>
 </td>
 </tr>
 <tr>
-<td>Account Number</td>
-<td><input type="text" size="15" maxlength="15" name="accno"></td>
+<td>Security Answer</td>
+<td><input type="text" size="20" maxlength="20" name="securityanswer"></td>
 </tr>
-<tr>
 <td colspan="2">
 <input type="checkbox" name="t_s"> I Agree to the <a href="terms.php">Terms and conditions</a>
 </td>
