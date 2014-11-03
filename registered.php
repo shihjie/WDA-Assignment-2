7a28e61cc7327b9793f62a5f201f8421c4f2d5e2
shihjie <shihjie1123@hotmail.com>
2014-11-03 22:57:56 +0800

    Update registered.php

--- registered.php
+++ registered.php
@@ -2,15 +2,37 @@
 <?php include 'connect.php';
 $username=$_POST['username'];
 $password=$_POST['pwconfirm'];
+$fname=$_POST['fname'];
+$lname=$_POST['lname'];
 $email=$_POST['email'];
+$address=$_POST['address'];
 $number=$_POST['number'];
-$paymentmethod=$_POST['paymentmethod'];
-$accno=$_POST['accno'];
+$securityquestion=$_POST['securityquestion'];
+$securityanswer=$_POST['securityanswer'];
+$rating=0.3
+date_default_timezone_set("Asia/Kuala_Lumpur");
+$register_date=date("Y-m-d");
+$countidquery= "SELECT COUNT (user_id) FROM user";
+$userid="U".$countidquery;
 
-$query = "INSERT INTO user (username, password, email, number, paymentmethod, accno)
-		  VALUES ('".$username."', '".$password."', '".$email."', '".$number."', '".$paymentmethod."', '".$accno."')";
+$query = "INSERT INTO user (user_id, first_name, last_name, username, password, email, address, phone_number, rating, register_date)
+		  VALUES ('".$userid."', '".$fname."', '".$lname."', '".$username."', '".$pwconfirm."', '".$email."', '".$address."', '".$number."', '".$rating."', '".$register_date."')";
+		  
+$query2 = "INSERT INTO recovery (user_id, secret_question, secret_answer)
+			VALUES ('".$userid."', '".$securityquestion."', '".$securityanswer."')";
 
 if(!mysqli_query($connection, $query))
+{
+die('Error: '.mysqli_error($connection));
+header("Refresh:5;url=register.php");
+echo "<font color='red'>Invalid Submission!<br/>";
+echo "You will be redirected to the register page in 5 seconds<br/>";
+echo "If the browser does not automatically redirect,<br/>";
+echo "Click here to <a href='register.php'/>register</a> as our member!</font>";
+}
+else
+{
+if(!mysqli_query($connection, $query2))
 {
 die('Error: '.mysqli_error($connection));
 header("Refresh:5;url=register.php");
@@ -25,6 +47,7 @@ header("Refresh:5;url=home.php");
 echo "	Thank you for registering!<br/>";
 echo "If the browser doesn't redirect you to home after 5 seconds,<br/>";
 echo "Click here to return to the <a href='home.php'/>Home Page</a>";
+}
 }
 
 
