01c4939abaf941851b69913ff8323d14db098ee7
shihjie <shihjie1123@hotmail.com>
2014-11-11 19:06:47 +0800

    Update bid.php
    major update..view updatelog.txt

--- bid.php
+++ bid.php
@@ -8,6 +8,7 @@
 
 <?php include_once("connect.php");
 include_once("expiry.php");
+include_once("search.php");
 session_start();
 if(isset($_SESSION['loggedid']))
 {
@@ -22,7 +23,7 @@ $runconfirmation=mysqli_query($connection, $confirmationquery)
 or die ("Error: ".mysqli_error($connection));
 $validatename=mysqli_fetch_row($runconfirmation);
 
-$bidamount="SELECT highest_bid FROM item WHERE item_id='".$itemid."'";
+$bidamount="SELECT highest_bid, status FROM item WHERE item_id='".$itemid."'";
 $runbidamount=mysqli_query($connection, $bidamount)
 or die ("Error: ".mysqli_error($connection));
 $validateamount=mysqli_fetch_row($runbidamount);
@@ -30,6 +31,8 @@ $validateamount=mysqli_fetch_row($runbidamount);
 if($validatename[0]!=$loggedid)
 {
 if($validateamount[0]<$bidprice)
+{
+if($validateamount[1]!="Inactive")
 {
 $updatebidquery="UPDATE item SET highest_bid='".$bidprice."', total_bid=total_bid+1 WHERE item_id='".$itemid."'";
 $updatebid=(mysqli_query($connection, $updatebidquery))
@@ -54,6 +57,15 @@ else
 	echo "You will be redirected to the Item bidding page in 5 seconds<br/>";
 	echo "If the browser does not automatically redirect,<br/>";
 	echo "Click here to <a href='bid.php'/>Bid</a> another item!</font>";
+}
+}
+else
+{
+header("Refresh:5;url=bid.php");
+echo "<font color='red'>You can't place a bid that is expired!<br/>";
+echo "You will be redirected to the bidding page in 5 seconds<br/>";
+echo "If the browser does not automatically redirect,<br/>";
+echo "Click here to <a href='bid.php'/>Bid</a>.</font>";
 }
 }
 else
@@ -98,34 +110,23 @@ else
 }
 
 
+}
+}
 
-}
-}
-else
-{
-header("Refresh:5;url=login.php");
-echo "<font color='red'>Please log in before you place a bid!<br/>";
-echo "You will be redirected to the login page in 5 seconds<br/>";
-echo "If the browser does not automatically redirect,<br/>";
-echo "Click here to <a href='login.php'/>Log in</a>.</font>";
-}
 ?>
 
 <?php
-if(isset($_SESSION['loggedid']))
-{
-$loggedid=implode($_SESSION['loggedid']);
+
 
 $highestbidder;
 $bidtime;
 
 
 $query = "SELECT item.item_name, item.item_description, category.category_name, item.post_date,
-		  item.status, user.username, item.starting_price, item.highest_bid, item.total_bid, item.end_time, item.item_id
+		  item.status, user.username, item.starting_price, item.highest_bid, item.total_bid, item.end_time, item.item_id, user.user_id
 		  FROM item 
 		  INNER JOIN category ON item.category_id=category.category_id
-		  INNER JOIN user ON item.user_id=user.user_id
-		  WHERE item.user_id!='".$loggedid."'";
+		  INNER JOIN user ON item.user_id=user.user_id";
 
 $runquery = mysqli_query ($connection, $query)
 or die("Error: ".mysqli_error($connection));
@@ -152,7 +153,9 @@ $bidtime=$trackbid[1];
 echo "<form id='form".$row[10]."' name='form".$row[10]."' action='' method='get'>";
 echo "<fieldset>";
 echo "<table>";
-echo "<tr><td><img src='images/".$row[10].".jpg' alt='property".$row[10]."'/></td></tr>";
+$imagepathing = glob("images/".$row[10].".*");
+$imagepathing = implode($imagepathing);
+echo "<tr><td><img src='".$imagepathing."' alt='property".$row[10]."'/></td></tr>";
 echo "<tr><td>Name: </td><td>".$row[0]."</td></tr>";
 echo "<tr><td>Description: </td><td>".$row[1]."</td></tr>";
 echo "<tr><td>Category: </td><td>".$row[2]."</td></tr>";
@@ -160,24 +163,39 @@ echo "<tr><td>Starting Date: </td><td>".$row[3]."</td></tr>";
 echo "<tr><td>Status: </td><td>".$row[4]."</td></tr>";
 echo "<tr><td>Owner: </td><td>".$row[5]."</td></tr>";
 echo "<tr><td>Starting Price: </td><td>".$row[6]."</td></tr>";
-echo "<tr><td>Highest Bid: </td><td>".$row[7]."</td><td>By ".$highestbidder." on ".$bidtime."</td></tr>";
+echo "<tr><td>Highest Bid: </td><td>".$row[7]."</td>";
+if(isset($_SESSION['loggedid']))
+{
+$loggedid=implode($_SESSION['loggedid']);
+if($row[11]!=$loggedid)
+{
+echo "<td>By ".$highestbidder." on ".$bidtime."</td></tr>";
+}
+}
 echo "<tr><td>Total Bids: </td><td>".$row[8]."</td></tr>";
 echo "<tr><td>Ending Date: </td><td>".$row[9]."</td></tr>";
 echo "<tr><td><input type='hidden' name='itemid' value='".$row[10]."'></td></tr>";
+if(isset($_SESSION['loggedid']))
+{
+if($row[11]!=$loggedid)
+{
 echo "<tr><td><input type='text' size='10' id='price".$row[10]."' name='bidprice'></td>";
 echo "<tr><td><input type='submit' value='Favourite' id='favourite' name='favourite'></td></tr>";
 echo "<tr><td><input type='submit' value='Bid' id='submit' name='submit'></td></tr>";
+}
+else
+{
+echo "<tr><td><font color='red'><b>You're the owner of this item.</b></td></tr>";
+}
+}
 echo "</table><br/>";
 echo "</fieldset>";
 echo "</form>";
 }
 }
 
-}
-else
-{
-header("Refresh:5;url=login.php");
-}
+
+
 ?>
 
     <?php //include_once("footer_template.php"); commented this out because I don't have the design file for this part yet.
