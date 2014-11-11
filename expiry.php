cbf6f0d006789c835bb96ad153feeabdfc2f6121
shihjie <shihjie1123@hotmail.com>
2014-11-11 12:30:43 +0800

    Update expiry.php
    fix bug

--- expiry.php
+++ expiry.php
@@ -1,13 +1,14 @@
 <?php
-$checkexpiry = "SELECT end_time from item";
+$checkexpiry = "SELECT end_time, item_id from item";
 
 $expquery = mysqli_query ($connection, $checkexpiry)
 or die("Error: ".mysqli_error($connection));
+$exprow=mysqli_fetch_row($expquery);
 while($exprow=mysqli_fetch_row($expquery))
 {
-if((date("Y-m-d h:i:sa"))<$exprow[0])
+if((date("Y-m-d h:i:sa"))>$exprow[0])
 {
-$expstatus="UPDATE item SET status ='Inactive'";
+$expstatus="UPDATE item SET status ='Inactive' WHERE item_id='".$exprow[1]."'";
 $updatestatus=(mysqli_query($connection, $expstatus))
 or die ("Error: ".mysqli_error($connection));
 }
