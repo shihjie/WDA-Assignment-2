970ab9cd9130d7b60979d3d4649d80e8017d9636
shihjie <shihjie1123@hotmail.com>
2014-11-11 19:08:53 +0800

    Create search.php
    as a feature in every page..to be include

---
+++ search.php
@@ -0,0 +1,6 @@
+<?php
+echo "<form id='searchform' name='searchform' action='searchresult.php' method='get'>";
+echo "<input type='text' size='30' maxlength='30' id='search' name='search' >";
+echo "<input type='submit' value='Search' id='s_submit' name='s_submit' >";
+echo "</form>";
+?>
